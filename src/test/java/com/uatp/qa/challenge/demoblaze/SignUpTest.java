b9c4afd09f55443ae1ee1b4347e460bb992b43ac
Sofia Ruiz <sofia.ruiz@distillery.com>
2022-09-20 01:56:33 -0300

    Adding chrome browser

--- src/test/java/com/uatp/qa/challenge/demoblaze/SignUpTest.java
+++ src/test/java/com/uatp/qa/challenge/demoblaze/SignUpTest.java
@@ -1,13 +1,17 @@
 package com.uatp.qa.challenge.demoblaze;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SignUpTest {
 
     @Test
     public void verifyHappyPathSignUp() {
-        throw  new UnsupportedOperationException("Error ok");
-
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.demoblaze.com/");
     }
 
 }
