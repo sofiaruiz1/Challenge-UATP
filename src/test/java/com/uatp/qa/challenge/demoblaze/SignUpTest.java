bb3c76999ad8060892878dc3b84f2a810b94fe20
Sofia Ruiz <sofia.ruiz@distillery.com>
2022-09-20 01:23:25 -0300

    TestNG and HappyPathSignOn configurations

---
+++ src/test/java/com/uatp/qa/challenge/demoblaze/SignUpTest.java
@@ -0,0 +1,13 @@
+package com.uatp.qa.challenge.demoblaze;
+
+import org.testng.annotations.Test;
+
+public class SignUpTest {
+
+    @Test
+    public void verifyHappyPathSignUp() {
+        throw  new UnsupportedOperationException("Error ok");
+
+    }
+
+}
